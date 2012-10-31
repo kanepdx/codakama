fd7acfa3cee11f09406628ee49ef70b6c42aef7d
Matthew Grimm <matthew.grimm@pdx.edu>
2012-10-31 08:40:40 -0700

    Loading new Lock Box folder for main with LCD libraries

---
+++ Coda Kama/Lock_Box/Lock_Box/Lock_Box.c
@@ -0,0 +1,23 @@
+/*
+ * Lock_Box.c
+ *
+ * 
+ *  Author: Coda Kama
+ */ 
+
+#define F_CPU 1000000			// 8Mhz/8 = 1Mhz = System clock
+
+#include <avr/io.h>				// AVR I/O library
+#include <util/delay.h>			// Delay library
+
+#include "LCD.h"				// LCD.h function library - Prototypes only
+#include "LCD.c"				// LCD.c function library - Functions only
+
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
