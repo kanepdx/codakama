c0f245fa2c9166b0bb593b2716308198128abf63
Colin Doolittle <colindoolittle@gmail.com>
2012-11-01 12:58:35 -0700

    removed 'main' and unnecessary .h headers

--- Team Test Code/colin/lockCode.c
+++ Team Test Code/colin/lockCode.c
@@ -5,16 +5,12 @@
  * Author: Colin Doolittle
  * Coda Kama Lock Box
  *
+ * Will require avr/io.h, util/delay.h, avr/interrupt.h
+ * 
  */ 
 
-#define F_CPU 1000000
-
-#include <avr/io.h>
-#include <util/delay.h>
-#include <AVR/interrupt.h>
-
 #define MOTORPORT PORTD			// what port is FIN on? PD4
-								// what port is RIN on? PD3
+					// what port is RIN on? PD3
 #define FDBKPORT PINB			// what port is FDBK on? PB6
 #define FDBKPIN PB6
 
@@ -22,7 +18,6 @@
 #define PUSHOUT 0b00001000		// RIN on PD3, |= with this to close
 
 #define BUFFER 250				// pause for 1/4 sec to allow motor to shove off
-#define DELAYTIME 5000			// just for testing, delete soon
 
 
 int unlock(int status)
@@ -63,29 +58,4 @@ int lock(int status)
 	MOTORPORT &= ~PUSHOUT;		// RIN goes low, lock stops
 	sei();						// re-enable interrupt
 	return status;				// update lock state
-}
-
-
-int main(void)
-{
-	DDRC = 0xFF;	// all pins are all outputs
-	DDRB = 0xBF;	// PB6 is an input
-	DDRD = 0xFF;	// all pins are outputs
-	
-	MOTORPORT = 0x0;	// all pins are low
-	PORTB = 0x0;	
-	PORTD = 0x0;
-	
-	int lock_state = 2;		// keeps track of lock state (0=open,1=locked,2=neither)
-
-    while(1)
-    {
-        // This loop will hopefully move the lock in and out
-		
-		lock_state = unlock(lock_state);
-		_delay_ms(DELAYTIME);
-		lock_state = lock(lock_state);
-		_delay_ms(DELAYTIME);
-		
-    }
 }
