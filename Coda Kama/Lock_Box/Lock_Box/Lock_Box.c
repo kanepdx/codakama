36901f85fc843ec0329de10e3d5d8b31d6d5e5c8
kanepdx <psu@kanetyler.com>
2012-11-10 23:35:53 -0800

    Eagle changes: 	Added missing ref. des. to several parts 	Minor component rearranging
    C changes:
    	Wrote most of the main loop.  There are few issues to
    	work through still and, obviously, testing still needed.

--- Coda Kama/Lock_Box/Lock_Box/Lock_Box.c
+++ Coda Kama/Lock_Box/Lock_Box/Lock_Box.c
@@ -1,4 +1,4 @@
-/*
+ /*
  * Lock_Box.c
  *
  * 
@@ -37,12 +37,13 @@ ISR(PCINT0_vect){
 	}
 }
 
-int main(void)
-{
+int main(void) {
 	// initialize
 	int rows[] = {ROW1, ROW2, ROW3, ROW4};
 	int cols[] = {COL1, COL2, COL3};
 	int count;											// counter variable used for loops
+	int count_queue;									// Keep track of how many characters are
+														// in the queue
 	int code_is_correct;								 
 	int lock_state = 2;									// initialized lock variable to intermediate status		
 	
@@ -56,59 +57,149 @@ int main(void)
 	// Initializing arrays 
 	int enter_code[16] = {'E','N','T','E','R',' ','C','O','D','E',' ',' ',' ',' ',' ',' '};
 	int incorrect_code[16] = {'I','N','C','O','R','R','E','C','T',' ','C','O','D','E',' ',' '};
-	int current_code[9] = {
+	int unlocked_menu_1[16] = {'#',' ','-',' ','T','O',' ','L','O','C','K',' ',' ',' ',' ',' ',};
+	int unlocked_menu_2[16] = {'*',' ','-',' ','S','E','T',' ','N','E','W',' ','C','O','D','E',};
+	int new_code_menu[16] = {'S','E','T',' ','N','E','W',' ','C','O','D','E',' ',' ',' ',' ',};
+	int cancel_code_change[16] = {' ',' ','-','-','C','A','N','C','E','L','E','D','-','-',' ',' ',};
+	int current_code[9];
 	
 	initRows(rows);										// set keypad rows as inputs
 	initColumns(cols);									// set keypad columns as outputs
 	initializeLCD();									// set up LCD and initialize in 4 bit mode
 	clearKeyQueue();
-	clearLCD();
-	
+	count_queue = 0;
+
+
+	// Ensure we're locked to start with.
+	while (lock_state != 1) {
+			lock_state = lock(2);
+	}
+
 	
 	// wait loop
-	while(1)											
-	{
+	while(1) {
+
 		// Print greeting
+		clearLCD();
 		writeLCDline(enter_code,1);
 		cursorPosition(2);
-		
-		// Wait for a key press
-		while(key_queue[0] == '\0');
-		
-		// Now that we have a key press we need to look at what was pressed.
-		// Is there first key in the queue a '#'...
-		if(key_queue[0] == '#'){
-			clearLCD();
-			clearKeyQueue();
-		
-		
-		// ...or is the first key in the queue a digit?...
-		} else if(key_queue[0] >= '0' && key_queue[0] <= '9'){
-			writeLCDcharacter(key_queue[0]);
-		
-		// ...or if first key in the queue isn't a digit or '#' then
-		// it must be the '*'
-		} else {
-			count = 0;
-			code_is_correct = 1;
-			// Checks the current queue code with the correct code
-			while(key_queue[count] != '\0'){
-				if(key_queue[count] != current_code[count])
-					code_is_correct = 0;				
-			}
-			if(!code_is_correct){
+
+		// While box is in locked state
+		while (lock_state == 1) {
+
+				// Wait for a key press
+				while(key_queue[count_queue] == '\0');
+				
+				// Now that we have a key press we need to look at what was pressed.
+				// Is the first key in the queue a '#'...
+				if(key_queue[0] == '#'){
+					clearLCD();
+					clearKeyQueue();
+					count_queue = 0;
+				
+				// ...or is the first key in the queue a digit?...
+				} else if(key_queue[0] >= '0' && key_queue[0] <= '9'){
+					// QUESTION: DO WE NEED TO MANUALLY SHIFT THE QUEUE??
+					writeLCDcharacter(key_queue[0]);
+					++count_queue;
+				
+				// ...or if first key in the queue isn't a digit or '#' then
+				// it must be the '*'
+				} else {
+					count = 0;
+					code_is_correct = 1;
+					// Checks the current queue code with the correct code
+					while(key_queue[count] != '\0'){
+						if(key_queue[count] != current_code[count])
+							code_is_correct = 0;				
+					}
+					if(!code_is_correct){
+						clearLCD();
+						writeLCDline(incorrect_code,1);
+						_delay_ms(5000);
+						clearKeyQueue();
+						count_queue = 0;
+						clearLCD();
+					}else{
+
+						// Unlock the box
+						lock_state = unlock(lock_state);
+					}
+				}		
+			} // End while (lock_state == 1)
+
+		// While box is in unlocked state
+			while (lock_state == 0 ) {
+
+				// Write unlocked menu
 				clearLCD();
-				writeLCDline(incorrect_code[],1);
-				_delay_ms(5000);
+				writeLCDline(unlocked_menu_1, 1);
+				writeLCDline(unlocked_menu_2, 2);
+
 				clearKeyQueue();
-				clearLCD();
-			}else{
-				lock_state = unlock(lock_state);
-			}
-		}		
-		
-	}
-}
+				count_queue = 0;
+
+				// Wait for keypress
+				while(key_queue[count_queue] == '\0');
+
+				// Now that we have a key press we need to look at what was pressed.
+				// Is the first key in the queue a '#'...
+				if(key_queue[0] == '#'){
+					lock_state = lock(lock_state);
+					clearKeyQueue();
+					count_queue = 0;
+
+				// ...or is the first key in the queue a '*'?
+				} else if(key_queue[0] == '*'){
+
+					clearLCD();
+					clearKeyQueue();
+					count_queue = 0;
+					writeLCDline(new_code_menu, 1);
+					cursorPosition(2);
+
+					// Now we start entering the new code. An '*' indicates that we're
+					// done entering the code.
+					while(key_queue[0] != '*') {
+						
+						clearKeyQueue();
+
+						// Wait for keypress
+						while(key_queue[count_queue] == '\0');
+
+						// Was the latest key pressed between 0 and 9, AND has the user
+						// entered less than the max number (8) of digits allowed for the
+						// code.
+						if(key_queue[0] >= '0' && key_queue[0] <= '9' && count_queue < 9){
+							// QUESTION: DO WE NEED TO MANUALLY SHIFT THE QUEUE??
+							writeLCDcharacter(key_queue[0]);
+							++count_queue;
+						
+						// If the latest key pressed is '*' then lets write the new
+						// code to eeprom
+						} else if (key_queue[0] == '*') {
+							writeTOeeprom();
+
+
+						// Otherwise the only key left is '#', so that's what must've been
+						// pressed. :)
+						} else {
+							clearLCD();
+							writeLCDline(cancel_code_change,1);
+							_delay_ms(5000);
+							clearKeyQueue();
+							count_queue = 0;
+							clearLCD();
+						}
+				
+					}
+
+					
+				}		
+			} // End while (lock_state == 0 )
+
+	} // End while(1);
+} // End main();
 
 void initRows(int r[]){									// sets keypad rows as inputs
 	for(int i = 0; i < NUM_ROWS; i++){
