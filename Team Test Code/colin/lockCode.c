2f9ccd42a7d2c6da09e43760c9c61fb9a73d2cf3
Colin Doolittle <colindoolittle@gmail.com>
2012-11-16 17:17:57 -0800

    Added 2nd feedback button

--- Team Test Code/colin/lockCode.c
+++ Team Test Code/colin/lockCode.c
@@ -11,33 +11,28 @@
 
 #define MOTORPORT PORTD			// what port is FIN on? PD4
 					// what port is RIN on? PD3
-#define FDBKPORT PINB			// what port is FDBK on? PB6
-#define FDBKPIN PB6
-
-#define PULLIN 0b00010000		// FIN on PD4, |= with this to open
-#define PUSHOUT 0b00001000		// RIN on PD3, |= with this to close
-
-#define BUFFER 250				// pause for 1/4 sec to allow motor to shove off
+#define FDBKLOCK PINB			// feedback pins are PB6 and PD2
+#define FDBKOPEN PIND			// PB6 limits the locking motion
+#define FDBKLOCKPIN PB6			// PD2 limits the opening motion
+#define FDBKOPENPIN PD2
 
 
 int unlock(int status)
 {
-	cli();							// disable interrupt
+	cli();				// disable interrupt
 	
-	if(status != 0)					// if not open, open
+	if(status != 0)				// if not open, open
 		MOTORPORT |= PULLIN;		// FIN goes high, lock shoves off
-	
-	_delay_ms(BUFFER);				// motor propagation delay
 	
 	while(status != 0){				// while not open
 									
-		if((FDBKPORT & (1<<FDBKPIN))==0)	// check for button press
-			status = 0;						// lock is 0=open
+		if((FDBKOPEN & (1<<FDBKOPENPIN))==0)	// check for button press
+			status = 0;			// lock is 0=open
 	}		
 
 	MOTORPORT &= ~PULLIN;		// FIN goes low, lock stops
-	sei();						// re-enable interrupt
-	return status;				// update lock state
+	sei();				// re-enable interrupt
+	return status;			// update lock state
 }
 
 int lock(int status)
@@ -45,17 +40,15 @@ int lock(int status)
 	cli();						// disable interrupt
 	
 	if(status != 1)					// if not locked, lock
-		MOTORPORT |= PUSHOUT;		// RIN goes high, lock shoves off
-	
-	_delay_ms(BUFFER);				// motor propagation delay
+		MOTORPORT |= PUSHOUT;			// RIN goes high, lock shoves off
 	
 	while(status != 1){				// while not open
 	
-		if((FDBKPORT & (1<<FDBKPIN))==0)	// check for button press
-			status = 1;						// lock is 1=locked
+		if((FDBKLOCK & (1<<FDBKLOCKPIN))==0)	// check for button press
+			status = 1;			// lock is 1=locked
 	}
 
 	MOTORPORT &= ~PUSHOUT;		// RIN goes low, lock stops
-	sei();						// re-enable interrupt
-	return status;				// update lock state
+	sei();				// re-enable interrupt
+	return status;			// update lock state
 }
