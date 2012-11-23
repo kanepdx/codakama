009d7aa68ea15f4ae53a3e3196e338bc54af5f04
Matthew Grimm <matthew.grimm@pdx.edu>
2012-11-22 19:37:54 -0800

    Cleaning code in Test file

---
+++ Team Test Code/matt/Lock_Box/Lock_Box/LCD.c
@@ -0,0 +1,135 @@
+/*
+ * LCD.c
+ *
+ *  Author: Coda Kama, Matt Grimm
+ *
+ * LCD.c - LCD functions  
+ *
+ */
+
+#include "LCD.h"
+
+#define BL PD1					// LCD back light LED Cathode- connected to PB1
+#define R_W PD0					// Read/Write (LCD) connected to PB0 (MC)
+#define RS PC5					// Register Select (LCD) connected to PC5 (MC)
+#define E PC4					// Enable (LCD) connected to PC5 (MC)
+#define LCD_PORT_1 PORTC		// Using Port C (MC) for the LCD
+#define LCD_DDR_1 DDRC			// With Port C (MC) being assigned direction register assignment
+#define LCD_PORT_2 PORTD		// Using Port B (MC) for the LCD
+#define LCD_DDR_2 DDRD			// With Port B (MC) being assigned direction register assignment
+
+
+// Sets up LCD and initializes it in 4 bit mode
+void initializeLCD(void){
+	int i = 0;						// counter variable for FOR loop
+	
+	LCD_DDR_1 = 0x3F;				// Making all LCD pins of port C outputs
+	LCD_PORT_1 &= ~(0x3F);			// Clearing LCD pins in port C
+	LCD_DDR_2 |= (1 << R_W);		// Making pin PB1 an output
+	LCD_PORT_2 &= ~(1 << R_W);		// Masking or clearing pin PB1 to be low for write operation
+	
+	/* 3 8-bit Function Set instructions to reset LCD */
+	for(i=0; i<=3; i++){
+		LCD_PORT_1 |= 0x3;
+		pulseElatch();
+		_delay_ms(1);
+		LCD_PORT_1 &= ~(0x3);
+	}
+	
+	/* Setting Function in 4-bit mode*/
+	LCD_PORT_1 |= 0x2;
+	pulseElatch();
+	_delay_ms(1);
+	LCD_PORT_1 &= ~(0x2);
+	
+	/* 4 BIT MODE NOW */
+
+	writeLCDdata(0x28); /* Setting Function Set (4 bit, Dual Line, 5x8 dots */
+	writeLCDdata(0x0F); /* Setting Display (Display ON, Cursor Blinking, Cursor Position ON */
+	writeLCDdata(0x06); /*  Setting Entry Mode (Increment after every read write op.*/
+}
+
+// Clears the LCD and places cursor at home (1x1)
+void clearLCD (void){
+	LCD_DDR_1 = 0x3F;				// Making LCD PORTC pins outputs
+	LCD_PORT_1 &= ~(1 << RS);		// Clearing RS to send command
+	LCD_DDR_2 |= (1 << R_W);		// Making pin PB0 an output
+	LCD_PORT_2 &= ~(1 << R_W);		// Masking or clearing pin PB0 to be low for write operation
+	writeLCDdata(0x01);				// Writing clear instruction
+}
+
+// Writes a character to current cursor position
+void writeLCDcharacter(int character){
+	LCD_DDR_1 = 0x3F;				// Making LCD PORTC pins outputs
+	LCD_PORT_1 |= (1 << RS);		// Setting RS to send data
+	LCD_DDR_2 |= (1 << R_W);		// Making pin PB0 an output
+	LCD_PORT_2 &= ~(1 << R_W);		// Masking or clearing pin PB0 to be low for write operation
+	writeLCDdata(character);		// Sending the character on the data lines
+}
+
+// Places the cursor to the home of the specified line
+void cursorPosition(int line){
+	int i = 1;
+	
+	LCD_DDR_1 = 0x3F;				// Making LCD PORTC pins outputs
+	LCD_PORT_1 &= ~(1 << RS);		// Clearing RS to send command
+	LCD_DDR_2 |= (1 << R_W);		// Making pin PB1 an output
+	LCD_PORT_2 &= ~(1 << R_W);		// Masking or clearing pin PB0 to be low for write operation
+	
+	writeLCDdata(0x0C);				// Turning the cursor OFF
+	
+	if (line == 2)					// Move cursor to line 2
+	{
+		writeLCDdata(0x2);			// Sending the cursor home
+		for(i=1;i<16+25;i++){		// Loop cursor move to the right instruction
+		writeLCDdata(0x14);
+		}
+	}
+	else
+	writeLCDdata(0x2);			// Sending cursor home
+
+	writeLCDdata(0x0F);				// Turning on the cursor ON
+}
+
+// Writes an array of 16 characters to the specified line
+void writeLCDline(char line_data[16], int line){
+	cursorPosition(line);					// Setting cursor position
+	for(int i=0; i<16; i++){				// Looping through character array
+		writeLCDcharacter(line_data[i]);	// Displaying character in specified array element
+	}
+}
+
+// Pulses Enable pin to latch in an instruction
+void pulseElatch(void){	
+	LCD_PORT_1 |= 1 << E;		// Setting Enable high
+	_delay_ms(1);				// Delaying 1 ms
+	LCD_PORT_1 &= ~(1 << E);	// Clearing Enable 
+}
+
+// Writes nibbles on data lines 
+void writeLCDdata(int data_byte){
+	int uppernibble = (data_byte & 0xF0) >> 4;	// Defining uppernibble to variable
+	int lowernibble = data_byte & 0x0F;			// Defining lowernibble to variable
+	LCD_PORT_1 &= ~0x0F;						// Clearing nibble on LCD
+	LCD_PORT_1 |= uppernibble;					// Sending uppernibble
+	pulseElatch();								// Latching in upper nibble
+	LCD_PORT_1 &= ~(uppernibble);				// Clearing uppernibble
+	LCD_PORT_1 |= lowernibble;					// Sending lowernibble
+	pulseElatch();								// Latching in lowernibble
+	_delay_ms(1);								// Delaying to ensure instruction is executed
+	LCD_PORT_1 &= ~(lowernibble);				// Clearing lower nibble
+}
+
+// Turns the LCD back light on or off
+void BacklightLCD (int on1_off0){
+	LCD_DDR_2 |= (1 << BL);		// Making pin PB1 an output
+	if (on1_off0 == 1)
+		LCD_PORT_2 &= ~(1 << BL);		// Masking or clearing pin PB1 sinking current turning on Back light
+	else
+		LCD_PORT_2 |= (1 << BL);		// Setting PB1 allowing no current flow to turn bakc light off
+	
+}
+
+
+
+
---
+++ Team Test Code/matt/Lock_Box/Lock_Box/keypad.c
@@ -0,0 +1,133 @@
+/*
+	this shit is working, kinda (best so far)
+*/
+#include "keypad.h"
+
+// includes to be removed later
+/*
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#define F_CPU 8000000UL
+*/
+
+// definitions to be moved later
+#define NUM_COLS 3										// number of columns on the keypad
+#define COL1 PINB3										// column 1 of keypad mapped to pin
+#define COL2 PINB4 										// column 2 of keypad mapped to pin
+#define COL3 PINB5 										// column 3 of keypad mapped to pin
+#define NUM_ROWS 4										// number of rows on the keypad
+#define ROW1 PINB0										// row 1 of keypad mapped to pin
+#define ROW2 PINB1										// row 2 of keypad mapped to pin
+#define ROW3 PINB2										// row 3 of keypad mapped to pin
+#define ROW4 PINB7										// row 4 of keypad mapped to pin
+#define STAR 10 										// '*'' key mapped to a decimal value
+#define POUND 12										// '#' key mapped to a decimal value
+#define KEY_QUEUE_SIZE 9								// size of key queue
+
+// globals to be moved later
+int lock_state = 1;										// will be updated by Colin's lock/unlock functions
+int key_queue[KEY_QUEUE_SIZE];							// holds user inputs
+int current_key;
+
+// adds the most recent key press to the key_queue
+void getKeyPress(void){		
+	int row;
+	int col;
+	int key;							
+	row = getRow();									// get the row of current key being pressed
+	col = getCol(row); 								// get the col of current key being pressed		
+	key = keyMap(row,col);							// will hold the key value to be added to queue
+	//pushKey(key); 									// push key onto queue		
+	current_key = key;
+}
+
+int getButtonState(){									// check rows, if any are low then a key is being pressed
+	_delay_us(100);
+	int s = 0;
+	if(!(PINB &(1 << ROW1))) s = 1;
+	else if(!(PINB &(1 << ROW2))) s = 1;				
+	else if(!(PINB &(1 << ROW3))) s = 1;				
+	else if(!(PINB &(1 << ROW4))) s = 1;
+	return s;
+}
+
+int getRow(void){										// determine which row on the keypad caused the interrupts.
+	int r;												// temporary placeholder for the row 
+		 if(!(PINB &(1 << ROW1))) r = ROW1;				// if row 1 is low then the key press was in row 1
+	else if(!(PINB &(1 << ROW2))) r = ROW2;				// if row 2 is low then the key press was in row 2	
+	else if(!(PINB &(1 << ROW3))) r = ROW3;				// if row 3 is low then the key press was in row 3
+	else if(!(PINB &(1 << ROW4))) r = ROW4;				// if row 4 is low then the key press was in row 4
+	return r;
+}
+
+int getCol(int r) {										// strobe outputs to determine column
+	int c;
+		
+	PORTB |= (1 << COL1);								// set first column high
+	_delay_us(30);										// wait for debouncing filter
+	if((PINB & (1 << r))) {								// if the row went high
+		c = 1;											// then the key press was in the first column
+	}
+	PORTB &= ~(1 << COL1);								// set first column back to low
+	
+	
+	PORTB |= (1 << COL2);								// set second column high
+	_delay_us(30);										// wait for denouncing filter
+	if((PINB & (1 << r))) {								// if the row went high
+		c = 2;											// then the key press was in the second column
+	}
+	PORTB &= ~(1 << COL2);								// set the second column back to low
+
+	
+	PORTB |= (1 << COL3);								// set third column high
+	_delay_us(30);										// wait for denouncing filter
+	if((PINB & (1 << r))) {								// if the row went high
+		c = 3;											// then the key press was in the third column
+	}
+	PORTB &= ~(1 << COL3);								// set the third column back to low
+
+	
+	return c;
+}
+
+int keyMap(int r,int c) { 								// maps (row,column) pairs to an integer value corresponding to a key
+	int k;
+														// adjust row value to account for 0 index
+		 if((r == ROW1) && (c == 1)) k = 0x31;
+	else if((r == ROW1) && (c == 2)) k = 0x32;
+	else if((r == ROW1) && (c == 3)) k = 0x33;
+	else if((r == ROW2) && (c == 1)) k = 0x34;
+	else if((r == ROW2) && (c == 2)) k = 0x35;
+	else if((r == ROW2) && (c == 3)) k = 0x36;
+	else if((r == ROW3) && (c == 1)) k = 0x37;
+	else if((r == ROW3) && (c == 2)) k = 0x38;
+	else if((r == ROW3) && (c == 3)) k = 0x39;
+	else if((r == ROW4) && (c == 1)) k = 0x2A;
+	else if((r == ROW4) && (c == 2)) k = 0x30;
+	else if((r == ROW4) && (c == 3)) k = 0x23;
+	return k;
+}
+
+int popKey(void){ 											// pop first element and shift queue;
+	int k = key_queue[0];
+	for(int i = 0; i < KEY_QUEUE_SIZE - 1; i++){
+		key_queue[i] = key_queue[i+1];
+	}	
+	key_queue[KEY_QUEUE_SIZE-1] = '\0';
+	return k;
+}
+
+void pushKey(int k){ 										// shift queue and update first element
+	for(int i = KEY_QUEUE_SIZE - 1; i > 0; i--){
+		key_queue[i] = key_queue[i-1];
+	}
+	key_queue[0] = k;		
+}
+
+void clearKeyQueue(void){ 										// clears submit_input, code_input, and input_index
+	for(int i = 0; i < KEY_QUEUE_SIZE; i++){
+		key_queue[i] = '\0';
+	}
+	
+}
