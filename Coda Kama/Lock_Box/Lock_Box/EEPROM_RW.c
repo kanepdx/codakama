d80e1cb860464382a1b026260b1c87b807a12919
Matthew Grimm <matthew.grimm@pdx.edu>
2012-11-25 22:39:21 -0800

    EEPROM_RW fix now fills any array with desired saved code and the rest NULL characters

--- Coda Kama/Lock_Box/Lock_Box/EEPROM_RW.c
+++ Coda Kama/Lock_Box/Lock_Box/EEPROM_RW.c
@@ -17,10 +17,16 @@ int short default_code[9] = {'0','0','0','0','\0','\0','\0','\0','\0'};		// Fact
 
 // Writes an 9 element array pass code to EEPROM
 void writeTOeeprom(int short code_to_save[]){
+	int ele_count = 0;													// variable declaration to loop and fill temp_code with desired saved code
 	int add_count = 0;													// variable declaration to loop through eeprom addresses
 	uint8_t temp_ee_address = EEPROM_DATA_ADDRESS;						// temp address variable to go through eeprom
-	while(code_to_save[add_count] != '\0'){								// looping through code to save array
-		eeprom_write_byte(temp_ee_address, code_to_save[add_count]);	// writing byte to specified address
+	int temp_code[9] = {'/0','/0','/0','/0','/0','/0','/0','/0','/0'};	// temp array initialized with null characters to hold the desired saved code
+	while(code_to_save != '\0'){										// This loop fills temp_code array with saved code and fills the rest wih NULL
+		temp_code[ele_count] = code_to_save[ele_count];	
+		ele_count++;													// incrementing array counter		
+	}
+	while(temp_code[add_count] != '\0'){								// looping through code to save array
+		eeprom_write_byte(temp_ee_address, temp_code[add_count]);		// writing byte to specified address
 		temp_ee_address++;												// incrementing address counter
 		add_count++;													// incrementing array counter
 	}
@@ -33,7 +39,7 @@ void writeTOeeprom(int short code_to_save[]){
 void readFROMeeprom(int short *saved_code[]){
 	int add_count = 0;														// variable declaration to loop through eeprom and elements of array
 	uint8_t temp_ee_address = EEPROM_DATA_ADDRESS;							// temp address variable to go through eeprom
-	
+		
 	if(eeprom_read_byte(EEPROM_NEWCODE_ADDRESS) == 0x0){
 		while(eeprom_read_byte(temp_ee_address) != NULL){					// looping through code to save array
 			saved_code[add_count] = eeprom_read_byte(temp_ee_address);		// writing byte to specified address
